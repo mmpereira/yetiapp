091459f3796483e1bedcca067a2f0cf76aa74de5
mmpereira_ <mario.mpereira@gmail.com>
2014-12-27 21:58:30 +0000

    fixed camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -61,39 +61,28 @@ angular.module('starter.controllers', [])
       
     })
 
-.controller('PhotoCtrl', function($scope, $cordovaCamera, $cordovaVibration) {
+.controller('PhotoCtrl', function($scope, $cordovaCamera) {
 
 
-  $scope.vibrate = function() {
-    
-    console.log('vibration: ', $cordovaVibration);
-    $scope.vibrating = "bzzzz";
-    $cordovaVibration.vibrate(100);
-  };
-
-  $scope.takePhoto = function() {
-  
-    var options = {
-      quality: 50,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.CAMERA,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 100,
-      targetHeight: 100,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: false
-    };
-  
-    $cordovaCamera.getPicture(options).then(function(imageData) {
-      // var image = document.getElementById('myImage');
-      $scope.image = "data:image/jpeg;base64," + imageData;
-      
-      
-    }, function(err) {
-      // error
-    });  
-  };
+  $scope.takePicture = function() {
+        var options = { 
+            quality : 75, 
+            destinationType : Camera.DestinationType.DATA_URL, 
+            sourceType : Camera.PictureSourceType.CAMERA, 
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+ 
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // An error occured. Show a message to the user
+        });
+    }
 
 })
 
